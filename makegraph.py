d305cb250d128d18ba2ea65bc5b3229986ace7aa
wisdomokoro1232 <122476723+wisdomokoro1232@users.noreply.github.com>
2023-01-28 13:38:11 +0000

    Adjusting for moisture sensor adn file reading

--- makegraph.py
+++ makegraph.py
@@ -1,18 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.arange(0.0, 10.0, 0.01)
-s = t**2 +2
+t = np.arange(-10.0, 10.0, 0.1)
+s = t**2 -5
 
-Max_saturation= max(t)
+Max_saturation= max(s)
 Q1_val = 0.25*Max_saturation
 Q2_val = 0.5*Max_saturation
 Q3_val= 0.75*Max_saturation
 
-Q1 = np.ma.masked_where(s < Q1_val, s)
-Q2 = np.ma.masked_inside( s,Q1_val, Q2_val)
-Q3 = np.ma.masked_inside(s,Q2_val , Q3_val)
-Q4= np.ma.masked_where((s>=Q3_val ), s)
+#Hides values greater than Q1
+Q1 = np.ma.masked_outside(s,0 , Q1_val)
+#Hides values OUTSIDE of Q1 and Q2
+Q2 = np.ma.masked_outside( s,Q1_val, Q2_val)
+#Hides Values outside of Q2 and Q3
+Q3 = np.ma.masked_outside(s,Q2_val , Q3_val)
+#Hides values less than Q3
+Q4= np.ma.masked_where((s<Q3_val ), s)
 fig, ax = plt.subplots()
-ax.plot(t, Q1, t, Q2, t, Q3, t, Q4)
+ax.plot(t, Q1, 'r',label= '<25% Saturation')
+ax.plot(t, Q2,'blue',label='25%<~<50% Saturation')
+ax.plot( t, Q3,'green',label='50%<~<75% Saturation')
+ax.plot( t, Q4,'purple',label='>75% Saturation')
+leg = ax.legend();
 plt.show()
