998a576120bfeb018cb077f68c8e8c4caa632377
wisdomokoro1232 <122476723+wisdomokoro1232@users.noreply.github.com>
2023-03-07 13:54:44 +0000

    Make stat model

--- makegraph.py
+++ makegraph.py
@@ -1,41 +1,49 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
-
 import seaborn as sns
 import pandas as pd
-import numpy as np
-from statsmodels.tsa import tsatools 
 
 #makes the plot come out in sns format
 sns.set()
-
+#%%
 #read table into python and duration coloumn
 table= pd.read_csv('Moisture.csv')
-Sensor_Val= table.loc[:,"Value"]
+Sensor_val= table.loc[:,"Moisture"]
 
-increment= input('How often were the values read (In seconds)? ')
+increment= int(input('How often were the values read (In seconds)? '))
 n_readings=len(Sensor_val)
 t = np.arange(0, n_readings*increment, increment)
 
+
+#%%
 Max_saturation= max(Sensor_val)
 Min_saturation= min(Sensor_val)
+#get a range of workign values 
 Saturation_range= Max_saturation-Min_saturation
 Q1_val = 0.25*Saturation_range +Min_saturation
 Q2_val = 0.5*Saturation_range + Min_saturation
 Q3_val= 0.75*Saturation_range + Min_saturation
 
+
+#%%
 #Hides values greater than Q1
-Q1 = np.ma.masked_outside(s,Min_saturation , Q1_val)
-#Hides values OUTSIDE of Q1 and Q2
-Q2 = np.ma.masked_outside( s,Q1_val, Q2_val)
+Q1 = np.ma.masked_outside(Sensor_val,Min_saturation , Q1_val)
+#Hides values OUTSIDE of Q1 and Q23
+Q2 = np.ma.masked_outside( Sensor_val,Q1_val, Q2_val)
 #Hides Values outside of Q2 and Q3
-Q3 = np.ma.masked_outside(s,Q2_val , Q3_val)
+Q3 = np.ma.masked_outside(Sensor_val,Q2_val , Q3_val)
 #Hides values less than Q3
-Q4= np.ma.masked_where((s<Q3_val ), s)
+Q4= np.ma.masked_where(Sensor_val <Q3_val , Sensor_val)
+
+#%%
 fig, ax = plt.subplots()
-ax.plot(t, Q1, 'r',label= '<25% Saturation')
-ax.plot(t, Q2,'blue',label='25%<~<50% Saturation')
-ax.plot( t, Q3,'green',label='50%<~<75% Saturation')
-ax.plot( t, Q4,'purple',label='>75% Saturation')
+ax.plot(t, Q4, 'r',label= '<25% Saturation')
+ax.plot(t, Q3,'blue',label='25%<~<50% Saturation')
+ax.plot( t, Q2,'green',label='50%<~<75% Saturation')
+ax.plot( t, Q1,'purple',label='>75% Saturation')
 leg = ax.legend();
+plt.xlabel("Time (s)")
+plt.ylabel("Moisture senseor output")
 plt.show()
+# %%
